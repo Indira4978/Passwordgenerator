0fba8cff65904dc9a7f4c412305aabe173fdcebf
Maho Yutopia <indira.salcede@gmail.com>
2023-11-25 16:52:05 +0000

    adds reviewed pseudo code

--- starter/script.js
+++ starter/script.js
@@ -88,40 +88,53 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-//Present a series of prompts for password criteria:
+///Step 1 : find out with prompt what does the user want to include in their password and validate each choice with an alert
+//          Prompt: "password length (min 8, max 128)"
+//          Alert: "password will be *** long"
+// If the user enters a number 8> or >128 - prompt again with red font
 
-//Length of password:
+// Other promtps :  Include Numbers ?
+//                  Include Uppercase ?
+//                  Include Lowercase ?
+//                  Include Special Characters ?
+//  If "OK" : alert "password will include ***" 
+//If "CANCEL" :  alert " password will not include ***"
 
-//At least 8 characters but no more than 128.
+//Step 2 : generate the password with the info collected in Step 1
+// can use "if statements"
+// if (number) select a random number from the number-array else // move on to check if  next condition is met //
 
-//Character types:
+//Step 3: display the password generated that is stored in the variable created in Stpe 2. 
 
-//Lowercase
-
-//Uppercase
-
-//Numeric
-
-//Special characters ($@%&*, etc.)
-
-//Code should validate for each input and at least one character type should be selected.
-
-//Once all prompts are answered, the password should be generated and displayed in an alert or written to the page.
-
+// We need an event listerner to call the function when the button is clicked
 
 
-//function getPasswordOptions() {
-  var selectpasswordLength = prompt("Enter password length.");
 
-  function selectCharacterTypes() {
+//var selectpasswordLength = prompt("Enter password length - 8 to 128 characters.");
+  
+  let selectpasswordLength;
+
+  function getPasswordOptions() {
+    //selectpasswordLength = prompt("Enter password length - 8 to 128 characters.");
+
+    // keep prompting the user to enter a number between8 and 128 untile they do. Once they odo, we can move to the next prompt
+    while (selectpasswordLength < 8 || selectpasswordLength > 128) {
+      selectpasswordLength = parseInt(prompt("Enter password length - 8 to 128 characters."));
+      alert("Enter password length - 8 to 128 characters.");
+      
+      // Prompt the user again to enter a valid password length
+
+      selectpasswordLength = prompt("Enter password length - 8 to 128 characters.");
+    }
+      //selectpasswordLength = prompt("Enter password length - 8 to 128 characters.");
+
     var takesLowercase = prompt("Include lowercase?"); 
     var takesUppercase = prompt("Include uppercase?"); 
     var takesNumber = prompt("Include numbers?"); 
     var takesSpecialCharacters = prompt("Include special characters?"); 
   }
   
-  selectCharacterTypes();
+  getPasswordOptions();
 
 // Function for getting a random element from an array
 function getRandom(arr) {
@@ -130,7 +143,9 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
+console.log("password button clicked ")
 
+return "Generated password field";
 }
 
 // Get references to the #generate element
